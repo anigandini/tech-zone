4230818b99dccc69702d0a5f7289409102bdf453
anigandini <anigandini@gmail.com>
2022-04-29 11:24:48 -0300

    adjust footer styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,32 +1,38 @@
-import img from '../assets/icons_sprite.webp'
 import styled from 'styled-components'
 
 const StyledFooter = styled.footer`
-    position: absolute;
-    bottom: 0;
     padding: 1rem;
     display: flex;
     justify-content: center;
     width: 100%;
 
     & a {
+        color: var(--color-neutrals-600);
         display: flex;
         gap: 0.5rem;
+
+        & .githubIcon {
+            width: 30px; 
+            height: 30px;
+            background: var(--icons-sprite) -53px -171px;
+        }
     }
 
     & a:hover {
         & .githubIcon {
             width: 30px; 
             height: 30px;
-            background: url(${img.src}) -158px -68px;
+            background: var(--icons-sprite) -158px -68px;
         }
+
+        background: var(--color-brand-default);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        font-size: 1.3rem;
+        font-weight: var(--font-weight-bold);
     }
 
-    & .githubIcon {
-        width: 30px; 
-        height: 30px;
-        background: url(${img.src}) -53px -171px;
-    }
+   
 `
 
 const Footer = () => {
